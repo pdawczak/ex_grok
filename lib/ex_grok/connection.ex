87ccb736cfa0dd88bfc671019b272a93920be2e2
Pawel Dawczak <pawel.dawczak@gmail.com>
2016-11-19 20:46:54 +0000

    Add documentation

--- lib/ex_grok/connection.ex
+++ lib/ex_grok/connection.ex
@@ -1,4 +1,8 @@
 defmodule ExGrok.Connection do
+  @moduledoc """
+  Provides information about connection establised by ngrok.
+  """
+
   defstruct http_url: nil, https_url: nil
 
   alias ExGrok.Connection
@@ -8,9 +12,16 @@ defmodule ExGrok.Connection do
     https_url: String.t
   }
 
+  @doc """
+  Returns new empty `ExGrok.Connection` struct.
+  """
   @spec new :: Connection.t
   def new, do: %__MODULE__{}
 
+  @doc """
+  Determines by information stored in `connection` if the ngrok connection has
+  been successfully established.
+  """
   @spec established?(Connection.t) :: boolean
   def established?(%__MODULE__{http_url: http_url, https_url: https_url}) do
     http_url != nil && https_url != nil
